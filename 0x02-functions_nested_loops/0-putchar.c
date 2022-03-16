57c639f712771e5498149fea0edcb4a5958efa6a
Chukwuma999 <chukwumaobinguma@gmail.com>
2022-03-16 23:35:01 +0100

    Very good work

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include "_putchar.h"
+
+/**
+ * main - prints _puutchar
+ *
+ * Description: prints _putchar
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	char text[10] = "_putchar";
+	int i = 0;
+
+	for (i = 0; i < 9; i++)
+	{
+		_putchar(text[i]);
+	}
+	_putchar('\n');
+
+	return (0);
+}
