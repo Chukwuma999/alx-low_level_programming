7f3b40017c187984954905ae2add04fbd40970e8
Chukwuma999 <chukwumaobinguma@gmail.com>
2022-03-17 23:29:56 +0100

    fixing code

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,51 +1,54 @@
-#include "main.h"
+#include "holberton.h"
 
 /**
-*print_times_table - Print the `n` times table, starting with 0.
-*
-*Description: If `n` is greater than 15 or less than 0, print nothing.
-*@n: int type number
-*/
+ * print_times_table - prints the times table
+ * @n: integer for which the times table will be printed
+ *
+ * Description: prints the times table
+ *
+ * Return: void
+ */
 
 void print_times_table(int n)
 {
-	int x = 0, y, z;
+	int row, column, product;
 
-	if (n > 15 || n < 0)
-	return;
-	while (x <= n)
+	if (n >= 0 && n < 15)
 	{
-	for (y = 0; y <= n; y++)
-	{
-	z = x * y;
-	if (z > 99)
-	{
-	_putchar(z / 100 + '0');
-	_putchar((z / 10 % 10) + '0');
-	_putchar(z % 10 + '0');
-	}
-	else if (z > 9)
-	{
-	_putchar(' ');
-	_putchar(z / 10 + '0');
-	_putchar(z % 10 + '0');
-	}
-	else if (y != 0)
-	{
-	_putchar(' ');
-	_putchar(' ');
-	_putchar(z + '0');
-	}
-	else
-	_putchar(z + '0');
-	
-	if (y != n)
-	{
-	_putchar(',');
-	_putchar(' ');
-	}
-	}
-	_putchar('\n');
-	x++;
+		for (row = 0; row <= n; row++)
+		{
+			for (column = 0; column <= n; column++)
+			{
+				product = row * column;
+
+				if (column == 0)
+					_putchar('0');
+				else if (product < 10)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(product % 10 + '0');
+				}
+				else if (product >= 10 && product < 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar((product / 10) % 10 + '0');
+					_putchar(product % 10 + '0');
+				}
+				else if (product > 99 && product < 1000)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(product / 100 + '0');
+					_putchar((product / 10) % 10 + '0');
+					_putchar(product % 10 + '0');
+				}
+			}
+			_putchar('\n');
+		}
 	}
 }
