6d57be37119a905611b3456c02e3d024e02bb1ac
Chukwuma999 <chukwumaobinguma@gmail.com>
2022-03-17 21:47:49 +0100

    fixing code

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -3,7 +3,6 @@
 /**
  * times_table -  a function that prints the 9 times table, starting with 0
  * rone = row, cone = column, d = digits of current result
- * 
  * Return: times table
  * add extra space past single digit
  */
