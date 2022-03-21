2271c516a1046552539438ce25bb308f730e5a84
Chukwuma999 <chukwumaobinguma@gmail.com>
2022-03-21 21:43:56 +0100

    Correct output trial

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -2,23 +2,20 @@
 
 /**
  * puts2 - prints one char out of 2
- * @str: type
- *
+ * @str: string
+ * Return: void
  */
 
 void puts2(char *str)
 {
-	int i, j = 0;
+	int i;
 
+	i = 0;
 	while (str[i] != '\0')
 	{
+		if (i % 2 == 0)
+			_putchar(str[i]);
 		i++;
-	}
-
-	while (j < i)
-	{
-		_putchar(str[j]);
-		j = j + 2;
 	}
 	_putchar('\n');
 }
